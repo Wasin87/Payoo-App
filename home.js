cb32b2fa2a5d8e609912210b407c90ee498fa751
Md Wasin Ahmed <wasinahmed87@gmail.com>
2025-09-02 23:08:32 +0600

    Second commit

--- home.js
+++ home.js
@@ -1,34 +1,34 @@
-const validPin = 5656 
+const validPin = 5656;
 
 document.getElementById('add-money-btn').addEventListener('click', function(e){
-    e.preventDefault()
-     //for amount number----------
-    const bank = document.getElementById('bank').value 
-     const accountNumber = document.getElementById('account-number').value 
+    e.preventDefault();
 
-     const amount = parseInt(document.getElementById('add-amount').value) 
+    const bank = document.getElementById('bank').value;
+    const accountNumber = document.getElementById('account-number').value;
+    const amount = parseInt(document.getElementById('add-amount').value);
+    const pin = parseInt(document.getElementById('add-pin').value);
+    const availableBalance = parseInt(document.getElementById('available-balance').innerText);
 
-     const pin = parseInt(document.getElementById('add-pin').value ) 
-     const availableBalance =  parseInt(document.getElementById('available-balance').innerText) 
-   
-if(accountNumber.length < 11){
-    alert('🧾 Please provide valid account number')
-    return;
-}
+    if (accountNumber.length < 11) {
+        alert('🧾 Please provide valid account number');
+        return;
+    }
 
-if(pin !== validPin){
-    alert('📌 Please provide valid pin number')
-    return;
-}
+    if (bank === 'Select bank') {
+        alert('🏦 Please select a bank');
+        return;
+    }
 
-if(pin !== validPin && accountNumber.length < 11){
-    alert('⚠️ Please provide valid account and pin number')
-    return;
-}
-     const totalNewAvailableBalance = amount + availableBalance
-     document.getElementById('available-balance').innerText = totalNewAvailableBalance
+    if (pin !== validPin) {
+        alert('📌 Please provide valid pin number');
+        return;
+    }
 
+    if (pin !== validPin && accountNumber.length < 11) {
+        alert('⚠️ Please provide valid account and pin number');
+        return;
+    }
 
-
-
-})
+    const totalNewAvailableBalance = amount + availableBalance;
+    document.getElementById('available-balance').innerText = totalNewAvailableBalance;
+});
