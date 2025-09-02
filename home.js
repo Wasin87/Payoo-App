2b7e31394f26409d1ef2dbf88aea208ea4c94f63
Md Wasin Ahmed <wasinahmed87@gmail.com>
2025-09-03 00:29:39 +0600

    Third commit

--- home.js
+++ home.js
@@ -1,5 +1,6 @@
 const validPin = 5656;
 
+//Add money features--------
 document.getElementById('add-money-btn').addEventListener('click', function(e){
     e.preventDefault();
 
@@ -24,11 +25,44 @@ document.getElementById('add-money-btn').addEventListener('click', function(e){
         return;
     }
 
-    if (pin !== validPin && accountNumber.length < 11) {
-        alert('⚠️ Please provide valid account and pin number');
-        return;
-    }
-
     const totalNewAvailableBalance = amount + availableBalance;
     document.getElementById('available-balance').innerText = totalNewAvailableBalance;
 });
+
+// Cash out features-------
+document.getElementById('Withdraw-btn').addEventListener('click', function(e){
+    e.preventDefault();
+
+    const agent = document.getElementById('agent-number').value;
+
+    const amount = parseInt(document.getElementById('Withdraw-amount').value);
+
+    const pin = parseInt(document.getElementById('Withdraw-pin').value);
+
+    const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+
+    if (agent.length < 11) {
+        alert('🧾 Please provide valid agent number');
+        return;
+    }
+
+    if (pin !== validPin) {
+        alert('📌 Please provide valid pin number');
+        return;
+    }
+
+    const totalNewAvailableBalance = availableBalance - amount ;
+    document.getElementById('available-balance').innerText = totalNewAvailableBalance;
+});
+
+
+// toggling features------
+document.getElementById('add-button').addEventListener('click',function(){
+   document.getElementById('cash-out-parent').style.display = "none"; 
+   document.getElementById('add-money-parent').style.display = "block";
+})
+
+document.getElementById('cash-out-button').addEventListener('click',function(){
+   document.getElementById('add-money-parent').style.display = "none"; 
+   document.getElementById('cash-out-parent').style.display = "block";
+})
